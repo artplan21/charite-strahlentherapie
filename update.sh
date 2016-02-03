bbdd0da01513d953775340b1abecbe26393d3a5d
Stephan Jorek <stephan.jorek@artplan21.de>
2016-02-03 16:45:34 +0100

    snapshot

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ wget --no-check-certificate --no-host-directories --mirror --base http://migrati
 for F in $( find . -type f -name "index.*" ) ; do sed -i.bak -e 's|_processed_|processed|g;s|_dummycontent|dummycontent|g;s|<meta name="robots" content="index,follow,noodp,noydir">|<meta name="robots" content="noindex,nofollow,noodp,noydir">|g;s|<base href="">|<base href="http://artplan21.github.io/charite-strahlentherapie/">|g;s|<base href="http://migration-strahlentherapie.charite.juno.artplan.local/">|<base href="http://artplan21.github.io/charite-strahlentherapie/">|g;s|https?://migration-strahlentherapie.charite.juno.artplan.local|http://artplan21.github.io/charite-strahlentherapie|g;s|href="/#|href="#|g;s|href="/|href="http://artplan21.github.io/charite-strahlentherapie/|g;s|?14........||g' "${F}" ; rm -v "${F}.bak" ; done
 for F in $( find . -type f -name "*?" | grep -E "\?[0-9]+$" ) ; do cp -v "${F}" "$(echo $F | sed -e 's|?.*$||g' )" ; done
 rm -v "+4930450589001"
+rm -v index.php*
 rm -r fileadmin/processed
 mv -v fileadmin/_processed_ fileadmin/processed
-rm -r fileadmin/user_upload/_dummycontent
+rm -r fileadmin/user_upload/dummycontent
 mv -v fileadmin/user_upload/_dummycontent fileadmin/user_upload/dummycontent
